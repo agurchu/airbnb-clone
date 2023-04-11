ac4b36a05a22375ab9d7768c5891f2f79b85343e
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-04-11 17:59:28 +0200

    initialize api with express

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -4,12 +4,12 @@ export default function RegisterPage() {
   return (
     <div className="mt-4 flex items-center justify-around grow">
       <div className="mb-52 ">
-        <h1 className="text-4xl text-center mb-4">Login</h1>
+        <h1 className="text-4xl text-center mb-4">Register</h1>
         <form className="mt-4 max-w-md mx-auto  ">
           <input type="text" placeholder="name" />
           <input type="email" placeholder="email address" />
           <input type="passward" placeholder="password" />
-          <button>Login</button>
+          <button>Register</button>
           <div className="my-4 text-center text-gray-500">
             Already a member?{" "}
             <Link className="underline text-black" to="/login">
