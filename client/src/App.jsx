a6503853e38608fd2f3881dbb21ece5145b6b517
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-04-11 16:31:30 +0200

    header component and react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,13 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import IndexPage from "./pages/IndexPage";
 
 function App() {
-  return <div className="bg-red-500">test</div>;
+  return (
+    <Routes>
+      <Route index element={<IndexPage />} />
+    </Routes>
+  );
 }
 
 export default App;
