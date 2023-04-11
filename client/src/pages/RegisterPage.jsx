041691157feaf422c7865fec0b0ee2b58d5eb669
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-04-11 17:39:59 +0200

    register page component

---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+export default function RegisterPage() {
+  return (
+    <div className="mt-4 flex items-center justify-around grow">
+      <div className="mb-52 ">
+        <h1 className="text-4xl text-center mb-4">Login</h1>
+        <form className="mt-4 max-w-md mx-auto  ">
+          <input type="text" placeholder="name" />
+          <input type="email" placeholder="email address" />
+          <input type="passward" placeholder="password" />
+          <button>Login</button>
+          <div className="my-4 text-center text-gray-500">
+            Already a member?{" "}
+            <Link className="underline text-black" to="/login">
+              Login
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
