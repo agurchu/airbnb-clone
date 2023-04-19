2482699f47fda6f6b8844cc4fffa9e4a234dcf02
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-04-19 14:23:25 +0200

    creating perks component, placePage states

--- client/src/pages/PlacePage.jsx
+++ client/src/pages/PlacePage.jsx
@@ -1,130 +1,40 @@
 import React, { useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import SubHeading from "../components/reusable/SubHeading";
+import Perks from "../components/reusable/Perks";
 
 export default function PlacePage() {
   const { action } = useParams();
+  const [title, setTitle] = useState("");
+  const [address, setAddress] = useState("");
+  const [addedPhotos, setAddedPhotos] = useState([]);
+  const [photoLink, setPhotoLink] = useState("");
+  const [description, setDescription] = useState("");
 
-  const [perks, setPerks] = useState([
-    {
-      name: "Wifi",
-      icon: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M8.288 15.038a5.25 5.25 0 017.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 011.06 0z"
-          />
-        </svg>
-      ),
-    },
-    {
-      name: "Free parking spot",
-      icon: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12"
-          />
-        </svg>
-      ),
-    },
-    {
-      name: "TV",
-      icon: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M6 20.25h12m-7.5-3v3m3-3v3m-10.125-3h17.25c.621 0 1.125-.504 1.125-1.125V4.875c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125z"
-          />
-        </svg>
-      ),
-    },
-    {
-      name: "Radio",
-      icon: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M3.75 7.5l16.5-4.125M12 6.75c-2.708 0-5.363.224-7.948.655C2.999 7.58 2.25 8.507 2.25 9.574v9.176A2.25 2.25 0 004.5 21h15a2.25 2.25 0 002.25-2.25V9.574c0-1.067-.75-1.994-1.802-2.169A48.329 48.329 0 0012 6.75zm-1.683 6.443l-.005.005-.006-.005.006-.005.005.005zm-.005 2.127l-.005-.006.005-.005.005.005-.005.005zm-2.116-.006l-.005.006-.006-.006.005-.005.006.005zm-.005-2.116l-.006-.005.006-.005.005.005-.005.005zM9.255 10.5v.008h-.008V10.5h.008zm3.249 1.88l-.007.004-.003-.007.006-.003.004.006zm-1.38 5.126l-.003-.006.006-.004.004.007-.006.003zm.007-6.501l-.003.006-.007-.003.004-.007.006.004zm1.37 5.129l-.007-.004.004-.006.006.003-.004.007zm.504-1.877h-.008v-.007h.008v.007zM9.255 18v.008h-.008V18h.008zm-3.246-1.87l-.007.004L6 16.127l.006-.003.004.006zm1.366-5.119l-.004-.006.006-.004.004.007-.006.003zM7.38 17.5l-.003.006-.007-.003.004-.007.006.004zm-1.376-5.116L6 12.38l.003-.007.007.004-.004.007zm-.5 1.873h-.008v-.007h.008v.007zM17.25 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zm0 4.5a.75.75 0 110-1.5.75.75 0 010 1.5z"
-          />
-        </svg>
-      ),
-    },
-    {
-      name: "Pets",
-      icon: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z"
-          />
-        </svg>
-      ),
-    },
-    {
-      name: "Private entrance",
-      icon: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
-          />
-        </svg>
-      ),
-    },
-  ]);
+  const [perks, setPerks] = useState([]);
+  const [extraInfo, setExtraInfo] = useState("");
+  const [checkIn, setCheckIn] = useState("");
+  const [checkOut, setCheckOut] = useState("");
+  const [maxGuests, setMaxGuests] = useState(1);
   const [checkTime, setCheckTime] = useState([
-    { title: "Check in time", placeholder: "14:00" },
-    { title: "Check out time", placeholder: "18:00" },
-    { title: "Max number of guests", placeholder: "3" },
+    {
+      title: "Check in time",
+      type: "text",
+      placeholder: "14:00",
+      input: { value: checkIn, setValue: setCheckIn },
+    },
+    {
+      title: "Check out time",
+      type: "text",
+      placeholder: "18:00",
+      input: { value: checkOut, setValue: setCheckOut },
+    },
+    {
+      title: "Max number of guests",
+      type: "number",
+      placeholder: "3",
+      input: { value: maxGuests, setValue: setMaxGuests },
+    },
   ]);
 
   return (
@@ -161,14 +71,29 @@ export default function PlacePage() {
               description=" Title for your place should be short and catchy as in
               advertisement"
             />
-            <input type="text" placeholder="title, e.g: My lovely apt" />
+            <input
+              value={title}
+              type="text"
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder="title, e.g: My lovely apt"
+            />
 
             <SubHeading title="Address" description=" Address to this place" />
-            <input type="text" placeholder="address" />
+            <input
+              value={address}
+              type="text"
+              onChange={(e) => setAddress(e.target.value)}
+              placeholder="address"
+            />
 
             <SubHeading title="Photos" description=" More = Better" />
             <div className="flex gap-2 items-center">
-              <input type="text" placeholder="Add using a link ....jpg" />
+              <input
+                value={photoLink}
+                type="text"
+                onChange={(e) => setPhotoLink(e.target.value)}
+                placeholder="Add using a link ....jpg"
+              />
               <button className="bg-gray-200 rounded-xl px-4 h-max py-3 font-medium">
                 Add&nbsp;photo
               </button>
@@ -198,26 +123,22 @@ export default function PlacePage() {
               title="Description"
               description="Description of the place"
             />
-            <textarea />
+            <textarea
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
             <SubHeading
               title="Perks"
               description="Select all the perks of your place"
             />
 
-            <div className="grid mt-4 gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
-              {perks.map((perk) => (
-                <label
-                  className="border p-4 rounded-xl flex items-center gap-2 cursor-pointer"
-                  key={perk.name}
-                >
-                  <input type="checkbox" />
-                  {perk.icon} <span>{perk.name}</span>
-                </label>
-              ))}
-            </div>
+            <Perks selected={perks} onChange={setPerks} />
 
             <SubHeading title="Extra Info" description="House rules, etc" />
-            <textarea />
+            <textarea
+              value={extraInfo}
+              onChange={(e) => setExtraInfo(e.target.value)}
+            />
 
             <SubHeading
               title="Check in & out time"
@@ -227,7 +148,12 @@ export default function PlacePage() {
               {checkTime?.map((check) => (
                 <div key={check.title}>
                   <h3 className="mt-3 -mb-1">{check.title}</h3>
-                  <input type="text" placeholder={check.placeholder} />
+                  <input
+                    type={check.type}
+                    value={check.input.value}
+                    onChange={(e) => check.input.setValue(e.target.value)}
+                    placeholder={check.placeholder}
+                  />
                 </div>
               ))}
             </div>
