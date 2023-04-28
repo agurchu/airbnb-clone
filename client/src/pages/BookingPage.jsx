00a4cc9c7ac1aca6a7b26d886eebfad0469a33ae
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-04-28 20:46:30 +0200

    BookingsPage & BookingPage

---
+++ client/src/pages/BookingPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+export default function BookingPage() {
+  const { id } = useParams();
+
+  return <div>BookingPage</div>;
+}
