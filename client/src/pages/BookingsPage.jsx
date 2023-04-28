f37664067f900aea85f01a10c0b474d7e9ebd2ef
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-04-28 21:18:31 +0200

    booking get in index,js

--- client/src/pages/BookingsPage.jsx
+++ client/src/pages/BookingsPage.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import AccountNav from "../components/AccountNav";
 
 export default function BookingsPage() {
-  return <div>BookingsPage</div>;
+  return (
+    <div>
+      <AccountNav />
+      <div></div>
+    </div>
+  );
 }
